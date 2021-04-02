0c27dedb602c07dff9eb29b4ca3ac6581b096c56
Edson Lucas <EDSONLUCASBD94@GMAIL.COM>
2021-04-01 21:48:23 -0300

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -35,7 +35,7 @@ export default function login() {
         <div className={styles.loginContainer}>
           <h1>Bem-vindo</h1>
           <div>
-            <p>Faça login com sua conta <strong>Github</strong> ou <strong>oogle</strong> para começar</p>
+            <p>Faça login com sua conta <strong>Github</strong> ou <strong>Google</strong> para começar</p>
           </div>
         </div>
 
